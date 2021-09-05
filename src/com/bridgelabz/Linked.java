90fd0afd1b37e0ed40e47b00431c166d97f05cd7
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-05 22:33:39 +0530

    welcome commit

---
+++ src/com/bridgelabz/Linked.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class Linked {
+    public static void main(String[] args ) {
+        //LinkedList<Integer> li = new LinkedList<>();
+        System.out.println("Welcome to Linked List:");
+    }
+}
