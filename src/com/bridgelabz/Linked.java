11069bada9a31ab0afb1c1c6416a1ff3aa66df54
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-05 22:37:04 +0530

    adding list

--- src/com/bridgelabz/Linked.java
+++ src/com/bridgelabz/Linked.java
@@ -4,7 +4,15 @@ import java.util.LinkedList;
 
 public class Linked {
     public static void main(String[] args ) {
-        //LinkedList<Integer> li = new LinkedList<>();
+        LinkedList<Integer> li = new LinkedList<>();
         System.out.println("Welcome to Linked List:");
+
+        li.add(56);
+        li.add(30);
+        li.add(70);
+
+        System.out.println(li);
+
+
     }
 }
