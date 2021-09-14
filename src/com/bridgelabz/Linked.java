c0f8166e602484f3a3e3cc4dc07650c28c797be4
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-14 21:01:36 +0530

    search a node

--- src/com/bridgelabz/Linked.java
+++ src/com/bridgelabz/Linked.java
@@ -6,29 +6,47 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Linked {
-<<<<<<< HEAD
-    static Node head;  //head of list
     //Node class
     static class Node {
         int data;
         Node next;
 
         //constructor to create a new node
-        Node(int data, Node next) {
+        Node(int data) {
             this.data = data;
-            this.next = next;
+            this.next = null;
+        }
+    }
+    //Represent the head and tail of the linked list
+    public Node head = null;
+    public Node tail = null;
+
+
+    //addNode() will add a new node to list
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+
+        //checks if te list is empty
+        if (head == null) {
+            //if list is empty, both head and tail will point to new node
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
         }
     }
 
 
     //function to insert node at the middle of the list
-  /*  static void insertAtMid(int x) {
+ /* static void insertAtMid(int data) {
         //if list is empty
         if (head == null)
-            head = new Node(x);
+            head = new Node();
         else {
             //get a new node
-            Node newNode = new Node(x);
+            Node newNode = new Node();
 
             Node ptr = head;
             int len = 0;
@@ -51,14 +69,19 @@ public class Linked {
         }
     }*/
     // function for display linked list
-    static void display(Node head) {
-        Node temp = head;
-        while (temp != null) {
-            System.out.println(temp.data +" ");
-            temp = temp.next;
+    public void display() {
+        Node current = head;
+        if (head == null) {
+            System.out.println("list is empty.");
+            return;
+        }
+        while (current != null) {
+            System.out.println(current.data +" ");
+            current = current.next;
         }
         System.out.println("null");
     }
+    //delete first Element from the List
     public static Node pop(Node headRef) {
         if (headRef == null) {
             return null;
@@ -68,79 +91,8 @@ public class Linked {
         System.out.println("the popped node is "+result);
         return headRef;
     }
-
-    //Driver program to test above
-    public static void main(String[] args ) {
-        /*creating list
-        head = null;
-        head = new Node(56);
-        head.next = new Node(70); */
-
-        // input keys
-        int[] keys = {56,30,70};
-
-        //point to the head node of the linked list
-        Node head = null;
-
-        //construct a linked list
-        for(int i = keys.length - 1; i >= 0; i--) {
-            head = new Node(keys[i], head);
-        }
-        head = pop(head);
-
-
-        //System.out.println("linked list before insertion:");
-        display(head);
-
-      /*  int x = 30;
-        insertAtMid(x);
-        System.out.println("linked list after insertion:");
-        display(); */
-
-=======
-    class Node {
-        int data;
-        Node next;
-
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-    public Node head = null;
-    public Node tail  = null;
-
-
-    //addNode() will add a new node to list
-    public void addNode(int data) {
-        Node newNode = new Node(data);
-
-        //checks if te list is empty
-        if (head == null) {
-            //if list is empty, both head and tail will point to new node
-            head = newNode;
-            tail = newNode;
-        }
-        else {
-            tail.next = newNode;
-            tail = newNode;
-        }
-    }
-
-
-    public  void display(){
-        Node current = head;
-        if (head == null) {
-            System.out.println("List is empty.");
-            return;
-        }
-        while (current!= null) {
-            System.out.print(current.data +" ");
-            current = current.next;
-        }
-        System.out.println();
-    }
-    public void pop () {
+    //delete last Element from the list
+    public void popFromLast () {
         if (head == null) {
             System.out.println("List is empty.");
             return;
@@ -161,7 +113,39 @@ public class Linked {
             }
         }
     }
+    //SearchNode will search for a given node in the list
+    public void searchNode(int data) {
+        Node current = head;
+        int i = 1;
+        boolean flag = false;
+        //checks whether list is empty
+        if (head == null) {
+            System.out.println("list is empty");
+        }
+        else {
+            while(current != null) {
+                //compares node to be found with each node present in the list
+                if (current.data == data) {
+                    flag = true;
+                    break;
+                }
+                i++;
+                current = current.next;
+            }
+        }
+        if (flag)
+            System.out.println("Element present in the at the position :"+i);
+        else
+            System.out.print("Element is not present in the list");
+    }
+
+
+    //Driver program to test above
     public static void main(String[] args ) {
+        /*creating list
+        head = null;
+        head = new Node(56);
+        head.next = new Node(70); */
         Linked li = new Linked();
         System.out.println("Welcome to Linked List:");
         //adds data to the list.
@@ -172,13 +156,33 @@ public class Linked {
         System.out.println("Original list");
         li.display();
 
+        li.searchNode(30);
+        //li.pop();
+        //System.out.println("Updated list");
+        //li.display();
 
-        li.pop();
-        System.out.println("Updated list");
-        li.display();
->>>>>>> deleteLastElement
+       /* // input keys
+        int[] keys = {56,30,70};
 
-       /* Iterator i = li.descendingIterator();
+        //point to the head node of the linked list
+        Node head = null;
+
+        //construct a linked list
+        for(int i = keys.length - 1; i >= 0; i--) {
+            head = new Node(keys[i], head);
+        }
+        head = pop(head);
+
+
+        //System.out.println("linked list before insertion:");
+        display(head);
+
+      /*  int x = 30;
+        insertAtMid(x);
+        System.out.println("linked list after insertion:");
+        display(); */
+
+        /* Iterator i = li.descendingIterator();
         System.out.println("Reversing list:");
         while (i.hasNext()) {
             System.out.println(i.next());
