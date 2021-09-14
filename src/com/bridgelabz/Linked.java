b782b75e3233201187bfc3630d9bb48e00cd24d4
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-14 17:36:07 +0530

    delete last element of list

--- src/com/bridgelabz/Linked.java
+++ src/com/bridgelabz/Linked.java
@@ -4,17 +4,84 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Linked {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public Node head = null;
+    public Node tail  = null;
+
+
+    //addNode() will add a new node to list
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+
+        //checks if te list is empty
+        if (head == null) {
+            //if list is empty, both head and tail will point to new node
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+
+    public  void display(){
+        Node current = head;
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        while (current!= null) {
+            System.out.print(current.data +" ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+    public void pop () {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+        else {
+            if(head!= tail) {
+                Node current = head;
+                //loop through the list till the second last element
+                while (current.next != tail) {
+                    current = current.next;
+                }
+                //Second last element will become new tail
+                tail = current;
+                tail.next = null;
+            }
+            else {
+                head = tail = null;
+            }
+        }
+    }
     public static void main(String[] args ) {
-        LinkedList<Integer> li = new LinkedList<>();
+        Linked li = new Linked();
         System.out.println("Welcome to Linked List:");
+        //adds data to the list.
+        li.addNode(56);
+        li.addNode(30);
+        li.addNode(70);
 
-        li.add(56);
+        System.out.println("Original list");
+        li.display();
 
-        System.out.println("list:"+li);
-        li.addLast(30);
-        li.addLast(70);
 
-        System.out.println("new list:"+li);
+        li.pop();
+        System.out.println("Updated list");
+        li.display();
 
        /* Iterator i = li.descendingIterator();
         System.out.println("Reversing list:");
